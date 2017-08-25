d0ba3a9c8f0de79dbeda5349bd4378f1caa7b672
Ivan <kyselovivan@gmail.com>
2017-08-25 23:12:00 +0300

    activities extends AppCompatActivity

--- app/src/main/java/com/example/ivan/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/ivan/criminalintent/CrimePagerActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.List;
 import java.util.UUID;
@@ -17,7 +18,7 @@ import java.util.UUID;
  * Created by ivan on 8/24/17.
  */
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID = "crime_id";
 
