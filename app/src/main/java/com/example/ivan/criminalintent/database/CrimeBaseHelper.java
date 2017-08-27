69af4830e3cc48a0da4d68cee81f910131485aa6
Ivan <kyselovivan@gmail.com>
2017-08-27 08:52:06 +0300

    CrimeBaseHelper added

---
+++ app/src/main/java/com/example/ivan/criminalintent/database/CrimeBaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.ivan.criminalintent.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by ivan on 8/27/17.
+ */
+
+public class CrimeBaseHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "crimeBase.db";
+
+    public CrimeBaseHelper(Context context) {
+        super(context, DATABASE_NAME,null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
