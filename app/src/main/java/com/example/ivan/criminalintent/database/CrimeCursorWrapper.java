aeff10bc5da2e5eaec15c300c91cfedf5535b152
Ivan <kyselovivan@gmail.com>
2017-08-29 13:37:55 +0300

    CrimeCursorWrapper class + gtCrime() added

---
+++ app/src/main/java/com/example/ivan/criminalintent/database/CrimeCursorWrapper.java
@@ -0,0 +1,25 @@
+package com.example.ivan.criminalintent.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import com.example.ivan.criminalintent.Crime;
+import com.example.ivan.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+/**
+ * Created by ivan on 8/29/17.
+ */
+
+public class CrimeCursorWrapper extends CursorWrapper {
+    public CrimeCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public Crime getCrime(){
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+        return null;
+    }
+}
