e88ef27de4ca0915b2ca8e36668587de72ab33ba
Ivan <kyselovivan@gmail.com>
2017-09-03 00:05:37 +0300

    added getPhotoFileName() to Crime & getPhotoFile() t0 CrimeLab

--- app/src/main/java/com/example/ivan/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/ivan/criminalintent/CrimeLab.java
@@ -4,11 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 
 import com.example.ivan.criminalintent.database.CrimeBaseHelper;
 import com.example.ivan.criminalintent.database.CrimeCursorWrapper;
 import com.example.ivan.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -68,6 +70,14 @@ public class CrimeLab {
         }finally {
             cursor.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime){
+        File externalFilesDir = mAppContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if(externalFilesDir == null){
+            return null;
+        }
+        return new File(externalFilesDir,crime.getPhotoFilename());
     }
 
     private static ContentValues getContentValues(Crime crime) {
